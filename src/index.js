0bdba7d40c113606ac91249289caad39591155d8
Johann Wagner <wagner@creatdev-solutions.com>
2019-07-05 19:22:23 +0200

    Added publicPath

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-const loaderFunction = (source) => {
+
+
+const loaderUtils = require('loader-utils');
+
+
+function loaderFunction(source) {
     // source should be an url from file-loader.
     // We want to load this url and return a promise with the content of the loaded file.
     // This file should be a json file, so we may want to parse this, too.
 
+    const options = loaderUtils.getOptions(this) || {};
     const [, filePath] = /.*"(.*)".*/.exec(source);
 
     console.log('Using environmentLoader for filePath', filePath);
 
+    const pubPath = `${options.publicPath || ''}${filePath}`;
+
+    console.log('Resolving to urlPath', pubPath);
+
     // We add Date.now() to our request to avoid caching from the browser.
     return `
-module.exports = fetch('${filePath}?id=${Date.now()}')
+module.exports = fetch(${pubPath} + '?id=' + Date.now())
   .then(function(response) {
     return response.json();
   })
     `;
-};
+}
 
 module.exports = loaderFunction;
